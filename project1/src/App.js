b849464f35fbb215c7c6431077f94cabe8e32e81
Yash Sharma <yash.arkedia@gmail.com>
2022-12-22 13:10:23 +0530

    Component is similar to function and returns HTML

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    [
+      <h1> Hello React... Array </h1>,
+      <h2> This if from H2 </h2>
+    ]
+    );
 }
 
 export default App;
